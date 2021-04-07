608297a864a2feb20c863464b2f1e1fb297d10bb
Robert Radzik <robertradzik@gmail.com>
2021-04-07 18:36:11 +0200

    first commit

---
+++ src/main/java/io/lorentez/roboticon/RoboticonApplication.java
@@ -0,0 +1,13 @@
+package io.lorentez.roboticon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboticonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoboticonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/lorentez/roboticon/RoboticonApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lorentez.roboticon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboticonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
