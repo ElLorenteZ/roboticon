5e3a8c4078c0ba0c6c3412fab9cebbf428c9429e
Robert Radzik <robertradzik@gmail.com>
2021-07-11 19:10:45 +0200

    feature: add request transfer robot functionality

---
+++ src/main/java/io/lorentez/roboticon/repositories/RobotTeamRepository.java
@@ -0,0 +1,9 @@
+package io.lorentez.roboticon.repositories;
+
+import io.lorentez.roboticon.model.RobotTeam;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RobotTeamRepository extends CrudRepository<RobotTeam, Long> {
+}
